0fef8810771b70b575c6d5eba290157434b13d3e
McK <maciej.mpluta@gmail.com>
2024-03-12 22:55:12 +0100

    MS-138 Created event editor component

---
+++ src/app/events/containers/event-editor/event-editor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-editor',
+  standalone: true,
+  imports: [],
+  templateUrl: './event-editor.component.html',
+  styleUrl: './event-editor.component.scss'
+})
+export class EventEditorComponent {
+
+}
