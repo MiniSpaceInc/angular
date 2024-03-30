ca0d66a8f982936d33538d99b0eb35a6c001731c
Bartosz Olszewski <bartoszoloolszewski@gmail.com>
2024-03-30 19:43:56 +0100

    draft event view

--- src/app/events/components/event-view/event-view.component.ts
+++ src/app/events/components/event-view/event-view.component.ts
@@ -1,11 +1,16 @@
 import { Component, inject, Input } from '@angular/core';
 import { EventFactory } from '../../../core/model/factory/EventFactory';
 import { Event } from '../../../core/model/Event';
+import { CardModule } from 'primeng/card';
+import { ToggleButtonModule } from 'primeng/togglebutton';
 
 @Component({
   selector: 'app-event-view',
   standalone: true,
-  imports: [],
+  imports: [
+    CardModule,
+    ToggleButtonModule,
+  ],
   templateUrl: './event-view.component.html',
   styleUrl: './event-view.component.scss'
 })
