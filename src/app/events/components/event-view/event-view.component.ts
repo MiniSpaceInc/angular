1eea8441024f62577c30bb4bb3e27e298a6dea51
Bartosz Olszewski <bartoszoloolszewski@gmail.com>
2024-03-30 21:10:22 +0100

    Added posts to event view

--- src/app/events/components/event-view/event-view.component.ts
+++ src/app/events/components/event-view/event-view.component.ts
@@ -3,6 +3,9 @@ import { EventFactory } from '../../../core/model/factory/EventFactory';
 import { Event } from '../../../core/model/Event';
 import { CardModule } from 'primeng/card';
 import { ToggleButtonModule } from 'primeng/togglebutton';
+import { PostMockService } from '../../../core/service/post/post-mock.service';
+import { Post } from '../../../core/model/Post';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-event-view',
@@ -10,10 +13,12 @@ import { ToggleButtonModule } from 'primeng/togglebutton';
   imports: [
     CardModule,
     ToggleButtonModule,
+    NgFor,
   ],
   templateUrl: './event-view.component.html',
   styleUrl: './event-view.component.scss'
 })
 export class EventViewComponent {
   @Input() event: Event = inject(EventFactory).createEmptyEvent();
+  @Input() posts: Post[] = inject(PostMockService).getPosts();
 }
