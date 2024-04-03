7ebc1f8ff7617fdd96815f0daa8d5c8111f0e7b0
Bartosz Olszewski <bartoszoloolszewski@gmail.com>
2024-04-03 19:16:59 +0200

    added new route for events

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { EventViewComponent } from "./events/components/event-view/event-view.component";
 import { EventsViewComponent } from "./events/containers/events-view/events-view.component";
 import { Routes } from '@angular/router';
 
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'events',
     component: EventsViewComponent
+  },
+  {
+    path: 'events/:id',
+    component: EventViewComponent
   }
 ];
