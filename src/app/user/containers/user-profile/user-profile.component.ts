54b84d30948e0367975e38f4c1200116cf957816
McK <maciej.mpluta@gmail.com>
2024-06-02 17:58:43 +0200

    MS-257 code refactoring

--- src/app/user/containers/user-profile/user-profile.component.ts
+++ src/app/user/containers/user-profile/user-profile.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {FriendsListComponent} from "../../components/friends-list/friends-list.component";
 import {InviteFriendComponent} from "../../components/invite-friend/invite-friend.component";
 import {FriendRequestsListComponent} from "../../components/friend-requests-list/friend-requests-list.component";
+import {EventInvitationsComponent} from "../../components/event-invitations/event-invitations.component";
+import {CardModule} from "primeng/card";
 
 @Component({
   selector: 'app-user-profile',
@@ -9,7 +11,9 @@ import {FriendRequestsListComponent} from "../../components/friend-requests-list
   imports: [
     FriendsListComponent,
     InviteFriendComponent,
-    FriendRequestsListComponent
+    FriendRequestsListComponent,
+    EventInvitationsComponent,
+    CardModule
   ],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.scss'
